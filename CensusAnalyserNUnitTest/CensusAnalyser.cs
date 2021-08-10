6ac0fd0cfeb955f0c78fb30ea6114661e6af5d1c
Neha121995 <jagtapneha1212@gmail.com>
2021-08-10 23:36:49 +0530

    CSV data files of Indian

---
+++ CensusAnalyserNUnitTest/CensusAnalyser.cs
@@ -0,0 +1,14 @@
+﻿using IndianStateCensusAnalyser.DTO;
+using System;
+using System.Collections.Generic;
+
+namespace CensusAnalyserNUnitTest
+{
+    internal class CensusAnalyser
+    {
+        internal Dictionary<string, CensusDTO> LoadCensusData(string indianStateCensusFilePath, object iNDIA, string indianStateCensusHeaders)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
